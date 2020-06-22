f0f9e61ed601ba20c17619c4a47fb38ca55345f3
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-06-21 20:56:33 -0400

    Changed port

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/productListing",(req,res) => {
 
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`The webserver is up and running`);
