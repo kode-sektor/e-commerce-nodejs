162f7412cd9ae05f24b089947c2021a120c29ed2
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-07-29 10:36:38 -0400

    Create User model

---
+++ models/Users.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const bcrypt = require("bcryptjs"); /*Import bcrypt*/
+
+/*createdBy and dateCreated are 2 important things when creating a DB.
+Just good practice*/
+
+const userSchema = new Schema({
+
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    dateCreated : {
+        type : Date,
+        default : Date.now()
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    profilePic : {
+        type : String 
+    }
+});
+
+// Double hashing is used 
+/*userSchema.pre("save", function(next) {
+    // salt random generated characters or strings 
+    bcrypt.genSalt(10).then((salt) => {
+        // this.password refers to the password in User.js/controllers (ln 20)
+        bcrypt.hash(this.password, salt).then((encryptPassword) => {
+            this.password = encryptPassword;
+            next();
+        }).catch(err => console.log(`Error occured when hashing ${err}`));
+    });
+
+});*/
+
+
+/*For every Schema you create (create a schema per collection), you must also create a model
+The model will allow you to perform CRUD operations on a given collection*/
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
