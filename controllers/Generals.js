8e170dbfc64749b79be81be00de1310006811ea4
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-08-03 14:53:11 -0400

    Displaying product by category: Fetch categories

--- controllers/Generals.js
+++ controllers/Generals.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 // Import schema
 const userModel = require("../models/Users");
+const categoryModel = require("../models/Categories");
+
 const path = require("path");	// For easy filename dismembering
 
 const bcrypt = require("bcryptjs");
@@ -15,17 +17,35 @@ const dashBoardLoader = require("../auth/authorisation");
 
 
 router.get('/', (req, res) => {
-    res.render("User/index", {
-        title : "Home Page",
-        /*data : bestSeller.getFeaturedProducts(),
-        dataCat : catProduct.getCategProducts()*/
-    });
+
+	categoryModel.find().then((categories) => {
+
+		// Fetch categories
+		const filteredCategory = categories.map( category => {
+			return {
+				category : category.title,
+			}
+		});
+
+		console.log ("CATEGORIES : " , filteredCategory);
+
+		res.render("User/index", {
+		    title : "Home Page",
+		    /*data : bestSeller.getFeaturedProducts(),
+		    dataCat : catProduct.getCategProducts()*/
+		});
+
+	}).catch((err) => {
+		console.log(`Error happened when pulling from the database : ${err}`);
+	});
+
 });
 
 // LOGOUT ROUTE
 router.get("/logout", (req, res) => {
 	req.session.destroy();
-	res.redirect("/user/login");
+/*	res.redirect("/user/login");
+*/	res.redirect("/user/login");
 });
 
 
