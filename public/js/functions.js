f2c67a43bfea34ea4ae0775e4b166c9b00c0a36a
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-08-15 22:40:41 -0400

    Redeploy to heroku

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,18 +1,11 @@
 	const methods = {
 
 		checkNull : (key, field, errors, loginVals) => {
-		    //(field == "") ? errors.null[`${key}`] = ' should not be empty' : loginVals[`${key}`] = field;
-
 		    if (field == "") {
 		    	errors.null[`${key}`] = ' should not be empty';
 		    } else {
 		    	loginVals[`${key}`] = field;
 		    }
-
-		    	console.log ("KEY : ", key);
-		    	console.log ("FIELD : ", field);
-		    	/*console.log ("ERRORS : ", errors);
-		    	console.log ("LOGINVALS : ", loginVals);*/
 		},
 
 		capitalise : (word) => {
